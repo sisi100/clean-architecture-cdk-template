670c1242d9f03d873224956bc07e3f82d674b86d
Shun Isii <sisii00.swork@gmail.com>
2021-02-28 15:32:39 +0900

    :bug:リージョンの指定方法を修正

--- app.py
+++ app.py
@@ -5,7 +5,7 @@ from aws_cdk import core
 from stacks import HogeAppStack, HogeInfraStack
 
 app = core.App()
-env = core.Environment(account=os.getenv("CDK_DEFAULT_ACCOUNT"), region=os.getenv("us-east-1"))
+env = core.Environment(account=os.getenv("CDK_DEFAULT_ACCOUNT"), region="us-east-1")
 
 # Stacks
 infra = HogeInfraStack(app, "hoge-infra-stack", env=env)
